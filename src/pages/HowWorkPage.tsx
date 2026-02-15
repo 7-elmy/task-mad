b9bfe8f2d04012705721c1753ee9ac14e10e4130
Ahmed Helmy <115832929+5elmy@users.noreply.github.com>
2026-02-15 22:11:30 +0200

    qwwe

--- src/pages/HowWorkPage.tsx
+++ src/pages/HowWorkPage.tsx
@@ -7,7 +7,7 @@ import InteractiveDemo from '@/components/howwork/InteractiveDemo'
 export default function HowWorkPage() {
   return (
     <div className='container mx-auto px-4 sm:px-6 md:px-16 lg:px-28'>
-      <div className='py-[40px]  md:py-[76px] '>
+      <div className='py-[40px]   md:py-[76px] '>
         <div className='grid grid-cols-1 gap-4 px-0 sm:px-4 md:px-6'>
           <h1 className='text-white font-sharp text-[32px] pb-[24px] md:pb-[64px]  lg:text-[64px]   font-semibold leading-none tracking-normal'>
             How We Work
@@ -35,10 +35,10 @@ export default function HowWorkPage() {
         <div   className='absolute inset-0  left-0 w-full'>
           <div style={{background:" linear-gradient(90deg, #433733 53.65%, rgba(106, 73, 36, 0.45) 74.28%)"}} className='flex flex-col  gap-8 items-start justify-center px-4 h-full w-full '>
 
-          <p className='font-sharp text-xl sm:text-2xl md:text-[32px] lg:text-[40px] font-semibold leading-none tracking-normal text-[#F36E3F]'>
+          <p className='font-sharp font-semibold text-[16px] leading-[16px] tracking-[0] text-center text-[#F36E3F]'>
             MadBoost.gg
           </p>
-          <p className='font-sharp text-xl sm:text-2xl  md:text-[32px] lg:text-[40px] pt-1 sm:pt-2 font-semibold leading-none tracking-normal text-white'>
+          <p className='font-sharp font-semibold text-[16px] leading-[16px] tracking-[0] text-center text-white'>
             Essential FAQs
           </p>
           </div>
